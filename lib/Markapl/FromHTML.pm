aade8b126f753709f6c09cb1657996a2f8d44674
Liu Kang-min <gugod@gugod.org>
2008-12-27 12:16:32 +0800

    html attributes are now converted.

--- lib/Markapl/FromHTML.pm
+++ lib/Markapl/FromHTML.pm
@@ -5,7 +5,7 @@ use strict;
 use 5.008;
 use Rubyish;
 use HTML::PullParser;
-# use Data::Dump qw(pp);
+use Data::Dump qw(pp);
 
 our $VERSION = '0.01';
 
@@ -33,7 +33,7 @@ def convert {
     while(my $token = $p->get_token) {
         # pp $token;
         if ($token->[0] eq 'S') {
-            push @stack, { tag => $token->[1], attr => $token->[2] };
+            push @stack, { tag => $token->[1], attr => [@$token[2..$#$token]]};
         }
         elsif ($token->[0] eq 'T') {
             unless($token->[1] =~ /^\s*$/s ) {
@@ -50,19 +50,27 @@ def convert {
                 $content = pop @stack;
             }
             my $start_tag = $content;
+            my $attr = "";
+            my @attr = @{$start_tag->{attr}};
+            if (@attr) {
+                while (my ($k, $v) = splice(@attr, 0, 2)) {
+                    $attr .= qq{ $k => "$v"};
+                }
+                $attr = "($attr )";
+            } 
 
             if (@content == 1) {
                 my $content_text = $content[0]->{code};
                 $content_text = "\"$content[0]->{text}\"" unless $content_text;
                 push @stack, {
-                    code => "$start_tag->{tag} { $content_text }"
+                    code => "$start_tag->{tag}${attr} { $content_text }"
                 };
             }
             else {
                 # pp @content;
                 my $content_code = join "\n", map { $_->{code} || $_->{text} } reverse @content;
                 push @stack, {
-                    code => "$start_tag->{tag} { $content_code }"
+                    code => "$start_tag->{tag}${attr} { $content_code }"
                 };
             }
         }
